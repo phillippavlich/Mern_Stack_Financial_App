37318e8a27298b94bae9b054d3b136c2af780185
phillippavlich <pavlicpm@mcmaster.ca>
2018-09-16 18:15:16 -0400

    Competed Tutorial and Connected Backend
    The back end has been attached to the front end. Now there will be changes to the structure and functionality of the application.

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavBar from './components/AppNavBar'
 import ShoppingList from './components/ShoppingList'
+import ItemModal from './components/ItemModal'
 
 import {Provider} from 'react-redux';
 import store from './store';
+import { Container} from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -13,12 +15,15 @@ class App extends Component {
       //this allows us to share state accross components
       <Provider store={store}>
         <div className="App">
-          <AppNavBar />
-          <h1>Financial Budgeting</h1>
-          <h2>Transaction</h2>
-          <h3>Income</h3>
-          <h3>Expense</h3>
-          <ShoppingList />
+            <AppNavBar />
+            <Container>
+            <ItemModal/>
+            <h1>Financial Budgeting</h1>
+            <h2>Transaction</h2>
+            <h3>Income</h3>
+            <h3>Expense</h3>
+            <ShoppingList />
+          </Container>
         </div>
       </Provider>
     );
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,14 +1,18 @@
-import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+//Makes changes to the data stored in the applications state to match server
+//import uuid from 'uuid';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 
 const initialState={
-	items: [
+	items: [],
+	loading: false
+
+	/*
 			{ id: uuid(), name: 'Eggs' },
 			{ id: uuid(), name: 'Milk' },
 			{ id: uuid(), name: 'Steak' },
 			{ id: uuid(), name: 'Water' }
-		]
+	*/	
 
 }
 
@@ -16,8 +20,27 @@ export default function(state=initialState, action){
 	switch(action.type){
 		case GET_ITEMS:
 			return {
-				...state
-			}
+				...state,
+				items: action.payload,
+				loading: false
+			};
+		case DELETE_ITEM:
+			return {
+				...state,
+				items: state.items.filter(item => item._id!==action.payload)
+			};
+		case ADD_ITEM:
+			return {
+				...state,
+				items: [action.payload, ...state.items]
+
+			};
+		case ITEMS_LOADING:
+			return {
+				...state,
+				loading: true
+
+			};
 		 default: 
 		 	return state;
 
